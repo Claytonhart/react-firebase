8c08922b892dcd72d0c80012d2c8470acf88c5b2
Clayton Hart <claytonhart123@yahoo.com>
2019-11-02 09:16:59 -0700

    withAuthorization hoc

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,11 +1,5 @@
-import React from 'react';
 import AuthUserContext from './context';
 import withAuthentication from './withAuthentication';
+import withAuthorization from './withAuthorization';
 
-const Session = () => {
-  return <div>Session</div>;
-};
-
-export default Session;
-
-export { AuthUserContext, withAuthentication };
+export { AuthUserContext, withAuthentication, withAuthorization };
