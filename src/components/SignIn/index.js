8fedab734c6c699b48de8be275194597ad74cd03
Clayton Hart <claytonhart123@yahoo.com>
2019-11-03 01:57:14 -0800

    Link all social logins & default pw + email login to the same account

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -25,6 +25,17 @@ const INITIAL_STATE = {
   error: null
 };
 
+// Social signin errors
+const ERROR_CODE_ACCOUNT_EXISTS =
+  'auth/account-exists-with-different-credential';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `
+An account with an E-Mail address to
+this social account already exists. Please login from
+this account instead and associate your social accounts on
+your personal account page.
+`;
+
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
@@ -99,6 +110,10 @@ class SignInGoogleBase extends Component {
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
+
         this.setState({ error });
       });
     event.preventDefault();
@@ -133,11 +148,15 @@ class SignInFacebookBase extends Component {
           roles: {}
         });
       })
-      .then(socialAuthUser => {
+      .then(() => {
         this.setState({ error: null });
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
+
         this.setState({ error });
       });
     event.preventDefault();
@@ -177,6 +196,10 @@ class SignInTwitterBase extends Component {
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
+
         this.setState({ error });
       });
     event.preventDefault();
